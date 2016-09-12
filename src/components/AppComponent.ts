408bc660aebf3977f56a98b4273e28ac5d489e86
Michael Mueller <michael.mueller@1und1.de>
2016-09-12 15:26:04 +0200

    renamed app tag

--- src/components/AppComponent.ts
+++ src/components/AppComponent.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
+    selector: 'app',
     template: '<h1>My First Angular 2 App</h1>'
 })
 export default class AppComponent { }
